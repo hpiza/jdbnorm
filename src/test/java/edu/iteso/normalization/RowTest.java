1b0a3e404e1bd33052a25f6cb803ea0f8508f9cd
hpiza <hpiza@iteso.mx>
2023-01-10 21:23:39 -0600

    Add first Row tests

---
+++ src/test/java/edu/iteso/normalization/RowTest.java
@@ -0,0 +1,53 @@
+package edu.iteso.normalization;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RowTest {
+
+    Row row1, row2;
+    String[] values;
+
+    @BeforeEach
+    void setUp() {
+        row1 = new Row();
+        values = new String[] {"v1", "v2", "v3"};
+        row2 = new Row(values);
+    }
+
+    @Test
+    void testRow() {
+        assertEquals(row1.size(), 0);
+        assertEquals(row2.size(), values.length);
+    }
+
+    @Test
+    void testGet() {
+        int i = 1;
+        String v = row2.get(i);
+        assertEquals(v, values[i]);
+    }
+
+    @Test
+    void testAdd() {
+        int s = row2.size();
+        String v1 = "v4";
+        row2.add(v1);
+        assertEquals(row2.size(), s + 1);
+        String v2 = row2.get(s);
+        assertEquals(v1, v2);
+    }
+
+    @Test
+     void testAddFirst() {
+        int s = row2.size();
+        String v1 = "v0";
+        row2.addFirst(v1);
+        assertEquals(row2.size(), s + 1);
+        String v2 = row2.get(0);
+        assertEquals(v1, v2);
+    }
+
+}
